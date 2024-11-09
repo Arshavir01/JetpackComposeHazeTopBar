ca79c563da16990d4378dbeb030c3870932f3dd3
Arshavir <arshavir.voskanyan@gmail.com>
2024-11-09 19:56:51 +0400

    create topbar

--- app/src/main/java/com/example/jetpackcomposehazetopbar/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposehazetopbar/MainActivity.kt
@@ -4,28 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
 import com.example.jetpackcomposehazetopbar.ui.theme.JetpackComposeHazeTopBarTheme
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JetpackComposeHazeTopBarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding))
+
+                val hazeState = remember { HazeState() }
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+                    state = rememberTopAppBarState()
+                )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = colorResource(id = R.color.black))
+                        .nestedScroll(scrollBehavior.nestedScrollConnection),
+                    containerColor = Color.Transparent,
+                    topBar = {
+                        TopBar(
+                            modifier = Modifier.hazeChild(hazeState),
+                            scrollBehavior = scrollBehavior
+                        )
+
+                        Pager(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .hazeChild(state = hazeState)
+                        )
+                    },
+                )
+                { innerPadding ->
+                   HomeScreen(modifier = Modifier.padding(innerPadding))
 
                 }
             }
         }
+    }
+
+    @Composable
+    fun HomeScreen(modifier: Modifier = Modifier) {
+
     }
 }
 
---
+++ app/src/main/java/com/example/jetpackcomposehazetopbar/Pager.kt
@@ -0,0 +1,16 @@
+package com.example.jetpackcomposehazetopbar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Pager(modifier: Modifier = Modifier) {
+    Column {
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+
+}
